74aec34d6fba643e43624825fa614fe58a496beb
b09901133 <b09901133@linux3.csie.ntu.edu.tw>
2023-06-04 17:03:03 +0800

    parsing

--- src/connect.py
+++ src/connect.py
@@ -1,6 +1,19 @@
 from pymongo import MongoClient
+import sys
+
+def input_parsing():
+    if len(sys.argv) != 3:
+        print("expected 2 additional arguments")
+        print(f"only get {len(sys.argv)}")
+        exit(0)
+
+    db_name = sys.argv[1]
+    feature = sys.argv[2]
+    connect(db_name)
 
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-print("Connection Successful")
-client.close()
+def connect(db_name):
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    print("Connection Successful")
+    
+    client.close()
