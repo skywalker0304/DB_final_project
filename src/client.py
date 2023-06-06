b46f336e1dc04c19edcc396200ff50caa0509011
Kevin Lin <kevin.lin@appier.com>
2023-06-06 19:01:21 +0800

    update client.py, add conenction

--- src/client.py
+++ src/client.py
@@ -14,32 +14,40 @@ print(f"Connected to server {ip_addr} on {port} port")
 while True:
     # Get user's request
     db = input("Enter the database you want to use")
+    collection = input("Enter the collection you want to use")
     user_input = input("Enter your request (predict, custom_operation): ")        
 
     if user_input == 'predict':
         preprocessing_methods_list = []
-        preprocessing_methods = input(
+        preprocessing_methods_string = input(
             "What kind of preprocessing methods do you want to use?\n"
-            "If you want to remove duplicates, please enter 1, "
-            "if you want to handle missing values, please enter 2.\n"
-            "It is also OK if you want to do both\n"
+            "If you want to remove duplicates, please enter 1, \n"
+            "If you want to handle missing values, please enter 2.\n"
+            "If you want to do feature scaling, please enter 3\n"
+            "It is also OK if you want to choose multiple options, "
+            "e.g. 123 means do all three, and 12 means do the first two\n"
             "If you don't want to use any, please enter None"
         )
 
-        if preprocessing_methods == "1":
-            preprocessing_methods_list = ["remove_duplicates"]
+        possible_list = ["1", "2", "3", "12", "13", "21", "23", "31", "32", "123", "132", "213", "231", "312", "321", "None"]
+        if preprocessing_methods_string not in possible_list:
+            print("Invalid request")
+            continue
 
-        elif preprocessing_methods == "2":
-            preprocessing_methods_list = ["handle_missing_values"]
-
-        elif preprocessing_methods == "12":
-            preprocessing_methods_list = ["handle_missing_values", "handle_missing_values"]
+        for ch in preprocessing_methods_string:
+            if ch == "1":
+                preprocessing_methods_list.append["remove_duplicates"]
+            if ch == "2":
+                preprocessing_methods_list.append["handle_missing_values"]
+            if ch == "3":
+                preprocessing_methods_list.append["feature_scaling"]
 
         predict_column = input("Enter the column you want to predict")
 
         # Prepare the request for prediction
         request = {
             'database': db,
+            'collection': collection,
             'request_type': 'predict',
             'preprocessing_methods': preprocessing_methods_list,
             'model': 'lightwood',
