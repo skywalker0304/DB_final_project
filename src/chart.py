69cc1a87a409088245ea99be988255b18ecf1cf5
Hank Huang <hank01217@gmail.com>
2023-06-08 21:33:05 +0800

    update chart: feature box chart

--- src/chart.py
+++ src/chart.py
@@ -34,3 +34,12 @@ def numeric_feature_barchart(df : pd.DataFrame, nf: list):
         sn.distplot(df[nf[i]],hist_kws=dict(edgecolor="black", linewidth=2), bins=10, color=list(np.random.randint([255,255,255])/255))
     plt.tight_layout()
     return plt.gcf()
+
+def numeric_feature_boxchart(df: pd.DataFrame, nf: list):
+    n = 5
+    plt.figure(figsize=[15,4*math.ceil(len(nf)/n)])
+    for i in range(len(nf)):
+        plt.subplot(math.ceil(len(nf)/3),n,i+1)
+        df.boxplot(nf[i])
+    plt.tight_layout()
+    return plt.gcf()
